d47d6a55e265c5897c023aa2db71441b997164b7
sasiSDN <sasisdn861999@gmail.com>
2024-08-14 10:18:43 +0530

    All the files for 13th Aug 2024

---
+++ src/exerice0813/LabHW1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+print("The maximum number is "f"{max(num1,num2)}")
+
+print("The power of "f"{num1}"" of " f"{num2}" " is:" f"{pow(num1,num2)}")
+print("The Sum of "f"{num1}"" and " f"{num2}" " is:",num1+num2)
+print("The Subtraction of "f"{num1}"" and " f"{num2}" " is:",num1-num2)
+print("The Multiplication of "f"{num1}"" and " f"{num2}" " is:",num1*num2)
+print("The Division of "f"{num1}"" and " f"{num2}" " is:",num1/num2)
